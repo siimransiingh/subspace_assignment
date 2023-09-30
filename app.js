d73f0727dba74047eecc0edad25d28b24189422f
Simran Singh <siimransiingh08@gmail.com>
2023-09-30 15:11:10 +0530

    memoize fubnction implemented

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 const app = express();
 app.use(express.json());
 
-const port = 3000; // Set your desired port
+const port = 3000;
 let blogDataPromise;
 
 const fetchBlogData = async () => {
@@ -22,61 +22,76 @@ const fetchBlogData = async () => {
 
   return response.json();
 };
+
+// Middleware to retrieve and cache blog data
+const getBlogData = async () => {
+  if (!blogDataPromise) {
+    blogDataPromise = fetchBlogData();
+  }
+
+  return blogDataPromise;
+};
+
+// Custom memoization function to cache analytics results
+const memoizedAnalytics = _.memoize(async () => {
+  const blogData = await getBlogData();
+
+  // Calculate analytics using Lodash
+  const totalBlogs = blogData.blogs.length;
+  console.log(totalBlogs);
+  const longestBlog = _.maxBy(blogData.blogs, 'title.length');
+  console.log(longestBlog);
+  const blogsWithPrivacy = blogData.blogs.filter(blog => blog.title.toLowerCase().includes('privacy'));
+  console.log(blogsWithPrivacy);
+  const uniqueBlogTitles = _.uniqBy(blogData.blogs, 'title');
+  console.log(uniqueBlogTitles);
+
+  // Prepare and return the response JSON
+  return {
+    totalBlogs,
+    longestBlog: longestBlog ? longestBlog.title : null,
+    blogsWithPrivacy: blogsWithPrivacy.length,
+    uniqueBlogTitles: uniqueBlogTitles.map(blog => blog.title),
+  };
+}, () => 'cacheKey'); // Using a constant cache key
+
 // Middleware to retrieve and analyze blog data
 app.get('/api/blog-stats', async (req, res) => {
   try {
-    if (!blogDataPromise) {
-     // Fetch and store the Promise
-     blogDataPromise = fetchBlogData();
-  }
-  blogData = await blogDataPromise;
-     
-        // Calculate analytics using Lodash
-        const totalBlogs = blogData.blogs.length;
-        const longestBlog = _.maxBy(blogData.blogs, 'title.length');
-        const blogsWithPrivacy = blogData.blogs.filter(blog => blog.title.toLowerCase().includes('privacy'));
-        const uniqueBlogTitles = _.uniqBy(blogData.blogs, 'title');
-
-        // Prepare the response JSON
-        const blogStats = {
-          totalBlogs,
-          longestBlog: longestBlog ? longestBlog.title : null,
-          blogsWithPrivacy: blogsWithPrivacy.length,
-          uniqueBlogTitles: uniqueBlogTitles.map(blog => blog.title),
-        };
-
-        res.json(blogStats);
-      
-     
+    const analyticsResult = await memoizedAnalytics();
+    res.json(analyticsResult);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred while processing the request.' });
   }
 });
 
+// Custom memoization function to cache search results
+const memoizedSearch = _.memoize(async (query) => {
+  const blogData = await getBlogData();
+
+const matchingBlogs = blogData.blogs.filter(blog =>
+  blog.title.toLowerCase().includes(query.toLowerCase())
+);
+
+// Prepare and return the response JSON
+return {
+  query,
+  matchingBlogs,
+};
+}, (query) => query); // Using the query as the cache key
 
 // Middleware for blog search
 app.get('/api/blog-search', async (req, res) => {
   try {
-    const query = req.query.query; // Get the query parameter from the request
+    const query = req.query.query;
 
     if (!query || typeof query !== 'string') {
       return res.status(400).json({ error: 'Invalid query parameter' });
     }
 
-    const blogData = await fetchBlogData();
-    // Search blogs based on the query (case-insensitive)
-    const matchingBlogs = blogData.blogs.filter(blog =>
-      blog.title.includes(query)
-    );
-
-    // Prepare the response JSON
-    const searchResults = {
-      query,
-      matchingBlogs,
-    };
-
-    res.json(searchResults);
+    const searchResult = await memoizedSearch(query);
+    res.json(searchResult);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred while processing the request.' });
